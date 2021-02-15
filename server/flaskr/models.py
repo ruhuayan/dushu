ce79cfd22026c22d19f2067ee80a03e437675994
ruhuayan <ruhuayan@gmail.com>
2021-02-14 20:55:08 -0500

    add flaskr server application

---
+++ server/flaskr/models.py
@@ -0,0 +1,72 @@
+"""Data models."""
+from . import db
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    href = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    alphabet = db.Column(db.String(1), nullable=False)
+    description = db.Column(db.Text)
+    download_ebook_count = db.Column(db.Integer)
+    download_pdf_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime())
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+class BookSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Book
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    title = fields.String(required=True)
+    href = fields.String(required=True)
+    author = fields.String(required=True)
+    category = fields.String(required=True)
+    alphabet = fields.String(required=True)
+    created_at = fields.DateTime()
+
+class Chapter(db.Model):
+    __tablename__ = "chapters"
+    chapter_id = db.Column(db.Integer, primary_key=True, autoincrement=False,)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), primary_key=True, autoincrement=False)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime())
+    # book = db.relationship("Book", backref='book', lazy=True)
+    def __repr__(self):
+        return f'<Book {self.book_id} - Chapter {self.title}>'
+
+class ChapterSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Chapter
+        sqla_session = db.session
+    chapter_id = fields.Number(required=True)
+    book_id = fields.Number(required=True)
+    title = fields.String(required=True)
+    content = fields.String()
+    created_at = fields.DateTime()
+
+class Serie(db.Model):
+    __tablename__ = "series"
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    serie_title = db.Column(db.String(255), nullable=False)
+    href = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime())
+    def __repr__(self):
+        return f'<Book {self.book_id} - Serie {self.serie_title}>'
+
+class SerieSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Serie
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    book_id = fields.Number(required=True)
+    serie_title = fields.String(required=True)
+    href = fields.String(required=True)
---
+++ server/flaskr/routes.py
@@ -0,0 +1,61 @@
+from flask import request, jsonify, make_response
+from flask import current_app as app
+from .models import *
+
+@app.route('/api/')
+def index():
+    return 'Hello World!'
+
+@app.route('/api/books', methods = ['GET'])
+def get_books():
+    get_books = Book.query.all()
+    book_schema = BookSchema(many=True)
+    books = book_schema.dump(get_books)
+    return make_response(jsonify({"books": books}))
+
+@app.route('/api/most_downloaded_books', methods = ['GET'])
+def get_most_downloaded_books():
+    get_books = Book.query.order_by(Book.download_ebook_count.desc()).limit(10)
+    book_schema = BookSchema(many=True)
+    books = book_schema.dump(get_books)
+    return make_response(jsonify({"books": books}))
+
+@app.route('/api/search', methods = ['GET'])
+def search_books():
+    search_string = request.args.get('book').strip()
+    if not search_string:
+        return make_response(jsonify({"books": []}))
+
+    search = "%{}%".format(search_string)
+    get_books = Book.query.filter(Book.title.like(search) | Book.author.like(search)).all()
+    book_schema = BookSchema(many=True)
+    books = book_schema.dump(get_books)
+    return make_response(jsonify({"books": books}))
+
+@app.route('/api/books/<id>', methods = ['GET'])
+def get_book_by_id(id):
+    get_book = Book.query.get(id)
+    book_schema = BookSchema()
+    book = book_schema.dump(get_book)
+
+    get_series = Serie.query.filter_by(book_id=id)
+    serie_schema = SerieSchema(many=True)
+    series = serie_schema.dump(get_series)
+    if series:
+        return make_response(jsonify({"book": book, "series": series}))
+    return make_response(jsonify({"book": book}))
+
+@app.route('/api/books/<id>/read', methods = ['GET'])
+def get_chapters_by_book_id(id):
+    get_chapters = Chapter.query.filter_by(book_id=id).offset(0).limit(3)
+    chapter_schema = ChapterSchema(many=True)
+    chapters = chapter_schema.dump(get_chapters)
+    return make_response(jsonify({"chapters": chapters}))
+
+@app.route('/api/books/<book_id>/chapters/<chapter_id>', methods = ['GET'])
+def get_chapter_by_id(book_id, chapter_id):
+    get_chapter = Chapter.query.filter_by(book_id=book_id, chapter_id = chapter_id).first()
+    print(get_chapter)
+    chapter_schema = ChapterSchema()
+    chapter = chapter_schema.dump(get_chapter)
+    return make_response(jsonify({"chapter": chapter}))
