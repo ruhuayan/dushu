35f166cd13336e67c25f8015643f4d1d5b650de7
ruhuayan <ruhuayan@gmail.com>
2021-05-09 22:20:12 -0400

    python - remove template files

---
+++ python/delete_books.py
@@ -0,0 +1,7 @@
+import os
+
+os.chdir("../public/ebooks")
+for root, dirs, files in os.walk(".", topdown = False):
+   for f in files:
+       if not f.endswith(('.pdf','.mobi', '.html')):
+           os.remove(os.path.join(root, f))
